ff62025c7703180968ace4ab58bcd0b18669b102
Brady Gebbie <bjgebbie@gmail.com>
2022-11-05 17:48:59 -0500

    Cleaning up

--- src/upscaler.py
+++ src/upscaler.py
@@ -3,7 +3,6 @@ from scipy.io.wavfile import read, write
 import sys
 import numpy as np
 
-
 def main():
     wav_file = "./samples/"+sys.argv[1]
     # read audio samples
@@ -11,16 +10,20 @@ def main():
     # Getting amplitude data
     samplerate = input_data[0]
     Y = input_data[1][:, 0]
-    X = range(len(Y) - 2)
+    X = range(len(Y))
 
     f = s.CubicSpline(X, Y)
     data = np.array([0,0])
-    precision = 10
-    for x in range(7500):
+    precision = 4
+
+    for x in range(len(Y) - 5):
         data = np.vstack([data, [Y[x],Y[x]]])
+        
         for i in range(precision - 1):
-            k = f.interpolate(x, x + (1 / precision))
-            
+            c = (1 / precision)
+            k = f.interpolate(x, x + c + (1 / precision))
+            c = (1 / precision)
+            k = round(k,0)
             t = [k, k]
             data = np.vstack([data, t])
 

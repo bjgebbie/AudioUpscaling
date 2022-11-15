7a727cba60b61265509224d4e96b7e0610d7d4ce
Brady Gebbie <bgebbie2@huskers.unl.edu>
2022-11-14 19:17:13 -0600

    Big undo

--- src/drivers/show_graph.py
+++ src/drivers/show_graph.py
@@ -8,36 +8,20 @@ import numpy as np
 
 wav_file = "./samples/"+sys.argv[1]
 
-actual = "./samples/8kHz_sample.wav"
 # read audio samples
-
 input_data = read(wav_file)
-actual_data = read(actual)
 # Getting amplitude data
 data = input_data[1]
-adata = actual_data[1]
-
 x = range(len(data[:, 0]))
-ax = range(len(adata[:, 0]))
-
 y = data[:, 0]
-ay = adata[:, 0]
-
 x = np.asanyarray(x)
-ax = np.asanyarray(ax)
 
 smooth = make_interp_spline(x, y)
-asmooth = make_interp_spline(ax, ay)
 
-a = 100
-b = 25
-xa = np.linspace(x.min(), x.max(), len(x)*b)
-xa2 = np.linspace(ax.min(), ax.max(), len(ax)*b)
+xa = np.linspace(x.min(), x.max(), len(x)*25)
 ya = smooth(xa)
-ya2 = asmooth(xa2)
-plt.plot(x[89722:89822],y[89722:89822], color='b', marker='o', label='Discrete',linestyle = 'None',)
-
-plt.plot(xa[int(89722/10*b):int(89822/10*b)]*10,ya2[int(89722/10*b):int(89822/10*b)], color='g', label='Actual')
+plt.plot(x[:100],y[:100], color='b', marker='o', label='Discrete',linestyle = 'None',)
+plt.plot(xa[:100*25],ya[:100*25], color='g', label='Actual')
 plt.ylabel("Amplitude")
 plt.xlabel("Sample")
 plt.title("Wav File")
